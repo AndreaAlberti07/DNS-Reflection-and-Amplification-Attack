d5d57ca85ded414a04c049945733fbb77fbf959b
Sp00f-00 <111920957+Sp00f-00@users.noreply.github.com>
2023-05-25 11:57:51 +0200

    script combined

--- Scripts/combined.sh
+++ Scripts/combined.sh
@@ -9,40 +9,43 @@ num_queries=480
 # Define the delay between each measurement in seconds
 delay=0.5
 
-# Create a timestamped output file
-output_file="usage_$(date +"%Y%m%d%H%M%S").txt"
-
 # Loop to perform multiple queries
 for ((i=1; i<=$num_queries; i++))
 do
     # Run dig and capture the output
-    output=$(dig +stats @192.168.1.3 -t A $domain)
+    output=$(dig +stats @192.168.1.4 -t A $domain)
 
     # Extract the query time using text processing (e.g., using grep and awk)
     query_time=$(echo "$output" | grep "Query time:" | awk '{print $4}')
 
-    # Print the individual query time
-    echo "Query $i: $query_time msec"
+    # Get timestamp
+    timestamp=$(date +"%H:%M:%S")
 
-    # Optional: Store the query times in a file for further analysis
-    echo "$query_time" >> DDoS_query_times.txt
- 
- 
-    
     # Get CPU usage percentage
-    cpu_usage=$(top -bn1 | grep "%Cpu(s)" | awk '{print $2}')
+    cpu_usage=$(top -l 2 -n 0 | grep "CPU usage" | tail -n 1 | awk '{print $3}')
 
     # Get memory usage percentage
-    memory_usage=$(free -m | awk '/Mem:/ {print $3/$2 * 100}')
+    memory_usage=$(top -l 1 -s 0 | awk '/PhysMem/ {print $2}')
 
-    # Get timestamp
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-    # Append the measurement to the output file
-    echo "$timestamp;CPU: $cpu_usage%  Memory: $memory_usage%" >> "$output_file"
+    # Run ping command and store output in a variable
+    ping_output=$(ping -c 1 192.168.1.4)
 
+    # Extract the RTT time from the ping output using awk
+    rtt_time=$(echo "$ping_output" | awk '/time=/{print $7}' | cut -d '=' -f 2)
+
+    # Optional: Store the query times in a file for further analysis
+    echo "$timestamp; Query_time: $query_time; CPU: $cpu_usage%; Memory: $memory_usage; Ping: $rtt_time"
+    echo "$timestamp; Query_time: $query_time; CPU: $cpu_usage%; Memory: $memory_usage; Ping: $rtt_time" >> DDoS_query_times.txt
+ 
     # Delay before the next measurement
     sleep $delay
+
+
+
+    
+
+
   
 done
 
