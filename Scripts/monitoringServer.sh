26022f4bdc2f24d95d7159f745010ce153b24a98
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-06-07 09:25:45 +0200

    refactoring folders

--- Scripts/monitoringServer.sh
+++ Scripts/monitoringServer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Define the duration of the monitoring in seconds
-duration=240
+duration=480
 
 # Define the delay between each measurement in seconds
-delay=1
+delay=0.5
 
 # Create a timestamped output file
 output_file="usage_$(date +"%Y%m%d%H%M%S").txt"
 
 # Loop to perform measurements
-for ((i = 0; i <= $duration; i += delay)); do
+for ((i = 0; i <= $duration; i += 1)); do
     # Get CPU usage percentage
     cpu_usage=$(top -bn1 | grep "%Cpu(s)" | awk '{print $2}')
 
@@ -18,10 +18,10 @@ for ((i = 0; i <= $duration; i += delay)); do
     memory_usage=$(free -m | awk '/Mem:/ {print $3/$2 * 100}')
 
     # Get timestamp
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    timestamp=$(date +"%H:%M:%S")
 
     # Append the measurement to the output file
-    echo "$timestamp CPU: $cpu_usage%  Memory: $memory_usage%" >>"data/$output_file"
+    echo "$timestamp;CPU: $cpu_usage%;Memory: $memory_usage%" >>"../data/$output_file"
 
     # Delay before the next measurement
     sleep $delay
